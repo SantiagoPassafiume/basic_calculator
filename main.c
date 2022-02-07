45a4a1b26ef79879a2acb438538c29f482cbfe4a
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-06 20:22:05 -0900

    implement basic sum of 2 numbers

--- main.c
+++ main.c
@@ -3,4 +3,14 @@
 
 int main()
 {
+    int num1;
+    int num2;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("\nYour answer is: %d\n", num1 + num2);
+
+    return 0;
 }
