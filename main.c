9dd39ef90efc5e0d495a30e40475607af7837354
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-07 02:28:55 -0900

    implement error handling of division by zero inside switch statement

--- main.c
+++ main.c
@@ -24,7 +24,15 @@ double calculate(char operator, double num1, double num2)
     case '*':
         return num1 * num2;
     case '/':
-        return num1 / num2;
+        if (num2 != 0)
+        {
+            return num1 / num2;
+        }
+        else
+        {
+            printf("\nYou cannot divide by zero.\n");
+            return false;
+        }
     default:
         errorMessage();
     }
